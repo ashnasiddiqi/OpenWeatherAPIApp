c5e57305494e4d4693e14cd5d519427431e0b007
ashnasiddiqi <139082108+ashnasiddiqi@users.noreply.github.com>
2023-08-21 17:19:52 -0500

    completed weather app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "864a997e36mshdce3e7d899c12c6p1bc241jsnc49dcf289c30",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4555680038284f7386fbb8991196dc83";
